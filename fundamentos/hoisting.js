d02fb666c03349e89f95415345d6a575cfb2ca2c
Isaac <isaacnbgomes@gmail.com>
2020-06-19 14:26:40 -0300

    Add files via upload

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,9 @@
+console.log('a =', a)
+var a = 2 // Mesmo declarada depois, a primeira linha definirá 'a' como "undefined"
+console.log('a = ', a)
+
+/* Não funciona com let
+ * console.log('b =', b)
+ * let b = 2
+ * console.log('b =', b)
+ */
---
+++ fundamentos/parNomeValor.js
@@ -0,0 +1,23 @@
+// par nome/valor
+const saudacao = 'Opa' // contexto léxico 1
+
+function exec() {
+    const saudacao = 'Falaaa' // contexto léxico 2
+    return saudacao
+
+}
+
+// Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Isaac',
+    idade: '20',
+    peso: '70',
+    endereco: {
+        logradouro: 'Rua Muito Legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
