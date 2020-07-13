f2058eda3672d33c5ba3ba07d4dcc1f0fd577bc0
Isaac <isaacnbgomes@gmail.com>
2020-07-13 14:57:23 -0300

    Add files via upload

---
+++ funcao/callback1.js
@@ -0,0 +1,9 @@
+// O callback é uma função que é chamada quando determinado evento acontece
+const fabricantes = ["Mercedes", "Audi", "BMW"]
+
+function imprimir(nome, indice) {
+    console.log(`${indice + 1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => console.log(fabricante))
